d77e1184e1ccd61ef477fb26cdf1e6d2633c2dae
Julio Araya <julio.araya@khipu.com>
2020-09-07 22:43:43 -0300

    Updating protocol to 1.1.0

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,41 @@
-import {OperationFailedReasons, QuestionTypes, UIMessageTypes} from "./index";
+/**
+ * Initial communication
+ */
+type ClientConnectionType = 'KHIPU_HANDSHAKE' | 'CLIENT_HANDSHAKE' | 'HANDSHAKE'
+type UIMessageType = ClientConnectionType | PaymentIdType | PaymentIsRunningType | PaymentFinishedType
+interface TypedMessage { type: UIMessageType }
+interface KhipuHandshake extends TypedMessage { type: 'KHIPU_HANDSHAKE' }
+interface ClientHandshake extends TypedMessage{ type: 'CLIENT_HANDSHAKE', publicKey: string, clientUUID: string }
+interface HandshakeMessage extends TypedMessage {/* @deprecated, use KhipuHandshake / ClientHandshake instead */ type: 'HANDSHAKE', key?: string }
+type InitialCommunicationMessageType = KhipuHandshake | ClientHandshake | HandshakeMessage
 
-type CustomTypes = {
-  OperationFailedReason: OperationFailedReason
-}
+/**
+ * Payment initialization
+ */
+type DeprecatedPaymentIdType = 'PAYMENT_ID' | 'UPDATE_PAYMENT'
+type PaymentIdType = | 'PAYMENT_ID_QUESTION' | 'PAYMENT_ID_RESPONSE' | 'UPDATE_PAYMENT_QUESTION' | 'UPDATE_PAYMENT_RESPONSE' | DeprecatedPaymentIdType
+interface PaymentIdQuestionMessage extends TypedMessage { type: 'PAYMENT_ID_QUESTION' }
+interface PaymentIdResponseMessage extends TypedMessage { type: 'PAYMENT_ID_RESPONSE', paymentId: string }
+interface PaymentIdMessage extends TypedMessage{/* @deprecated, use PaymentIdQuestionMessage / PaymentIdResponseMessage instead. */ type: 'PAYMENT_ID', paymentId?: string }
+interface UpdatePaymentQuestionMessage extends TypedMessage { type: 'UPDATE_PAYMENT_QUESTION', email?: string, bankId?: string, banks: Choice[] }
+interface UpdatePaymentResponseMessage extends TypedMessage { type: 'UPDATE_PAYMENT_RESPONSE', email: string, bankId: string }
+interface UpdatePaymentMessage extends TypedMessage { /* @deprecated, use UpdatePaymentQuestionMessage / UpdatePaymentResponseMessage instead */ type: 'UPDATE_PAYMENT', email?: string, bankId?: string, banks?: Choice[] }
+type PaymentInitializationMessagesType =
+    | PaymentIdQuestionMessage
+    | PaymentIdResponseMessage
+    | PaymentIdMessage
+    | UpdatePaymentQuestionMessage
+    | UpdatePaymentResponseMessage
+    | UpdatePaymentMessage
 
+/**
+ * Payment process
+ */
+type PaymentIsRunningType = 'OPERATION_INFO' | 'PROGRESS_START' | 'PROGRESS_STOP' | 'FORM' | 'USER_RESPONSE'
+type PaymentFinishedType = 'OPERATION_SUCCESS' | 'OPERATION_WARNING' | 'OPERATION_FAILURE'
+interface BaseMessage { title: string }
 
-type UiMessage =
-    | FormMessage
+type PaymentProcessMessageType = | FormMessage
     | UIResponseMessage
     | InfoMessage
     | SuccessMessage
@@ -14,33 +43,16 @@ type UiMessage =
     | FailureMessage
     | ProgressStartMessage
     | ProgressStopMessage
-    | HandshakeMessage
-    | PaymentIdMessage
-    | UpdatePaymentMessage
 
-type UIMessageType = typeof UIMessageTypes[number]
-type QuestionType = typeof QuestionTypes[number]
-type OperationFailedReason = typeof OperationFailedReasons[number]
 
-interface TypedMessage {
-  type: UIMessageType
-}
 
-interface BaseMessage {
-  title: string
-}
+type UiMessage = InitialCommunicationMessageType | PaymentInitializationMessagesType | PaymentProcessMessageType
 
-interface FormMessage extends BaseMessage, TypedMessage {
-  id: string
-  info?: string
-  pageTitle?: string
-  continueLabel?: string
-  currentStep?: number
-  totalSteps?: number
-  errorMessage?: string
-  questions: Question[]
-  termsUrl?: string
-  type: 'FORM'
+type QuestionType = 'password' | 'number' | 'email' | 'rut' | 'input' | 'list' | 'coordinates'
+
+interface Choice {
+  value: string
+  name: string
 }
 
 interface Question {
@@ -59,21 +71,29 @@ interface Question {
   choices?: Choice[] | Array<any> | any
 }
 
-interface Choice {
-  value: string
-  name: string
-}
-
-interface UIResponseMessage extends TypedMessage {
-  type: 'USER_RESPONSE'
+interface FormMessage extends BaseMessage, TypedMessage {
   id: string
-  answers: UIAnswer[]
+  info?: string
+  pageTitle?: string
+  continueLabel?: string
+  currentStep?: number
+  totalSteps?: number
+  errorMessage?: string
+  questions: Question[]
+  termsUrl?: string
+  type: 'FORM'
 }
 
 interface UIAnswer {
   id: string
   value: string
   multiple: boolean
+}
+
+interface UIResponseMessage extends TypedMessage {
+  type: 'USER_RESPONSE'
+  id: string
+  answers: UIAnswer[]
 }
 
 interface InfoMessage extends BaseMessage, TypedMessage {
@@ -83,6 +103,16 @@ interface InfoMessage extends BaseMessage, TypedMessage {
 interface SuccessMessage extends ExitMessage, TypedMessage {
   type: 'OPERATION_SUCCESS'
 }
+
+type ReasonExecutionFailed = 'TASK_EXECUTION_ERROR' | 'TASK_DOWNLOAD_ERROR' | 'INVALID_OPERATION_ID'
+
+type ReasonFinishedType = 'NO_BACKEND_AVAILABLE' | 'TASK_FINISHED' | 'TASK_DUMPED'
+
+type ReasonInteractionType = 'FORM_TIMEOUT'
+
+type ReasonNotificationType = 'TASK_NOTIFICATION_ERROR'
+
+type OperationFailedReason = ReasonExecutionFailed | ReasonFinishedType | ReasonInteractionType | ReasonNotificationType
 
 interface WarningMessage extends ExitMessage, TypedMessage {
   failureReason?: OperationFailedReason
@@ -111,33 +141,4 @@ interface ProgressStopMessage extends ProgressMessage, TypedMessage  {
 
 interface ProgressMessage {
   title?: string
-}
-
-interface HandshakeMessage extends TypedMessage {
-  type: 'HANDSHAKE'
-  key?: string
-}
-
-interface PaymentIdMessage extends TypedMessage{
-  type: 'PAYMENT_ID'
-  /**
-   * @fromClient: The payment Id
-   */
-  paymentId?: string
-}
-
-interface UpdatePaymentMessage extends TypedMessage {
-  type: 'UPDATE_PAYMENT'
-  /**
-   * @fromClient: The user email
-   */
-  email?: string
-  /**
-   * @fromClient: The bank id selected
-   */
-  bankId?: string
-  /**
-   * @fromServer: The list of available banks
-   */
-  banks?: Choice[]
 }
